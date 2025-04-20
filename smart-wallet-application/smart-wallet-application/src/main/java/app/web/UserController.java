241019602b5a6e1c1a5c3aafa3e54c0b9c0253e8
Venelin Davidov <114108014+VenelinDavidov@users.noreply.github.com>
2025-04-20 21:18:22 +0300

    new updates for wallet app

--- smart-wallet-application/smart-wallet-application/src/main/java/app/web/UserController.java
+++ smart-wallet-application/smart-wallet-application/src/main/java/app/web/UserController.java
@@ -2,11 +2,11 @@ package app.web;
 
 import app.user.model.User;
 import app.user.service.UserService;
+import app.web.dto.UserEditRequest;
+import app.web.mapper.DtoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.UUID;
@@ -29,7 +29,7 @@ public class UserController {
 
 
 
-
+// Edit profile user
     @GetMapping("/{id}/profile")
     public ModelAndView getProfileMenu (@PathVariable UUID id){
 
@@ -38,8 +38,18 @@ public class UserController {
         ModelAndView modelAndView = new ModelAndView ();
         modelAndView.addObject ("user", user);
         modelAndView.setViewName ("profile-menu");
-
+        modelAndView.addObject ("userEditRequest", DtoMapper.mapUserToUserEditRequest(user));
 
         return modelAndView;
+    }
+
+
+
+    @PutMapping("/{id}/profile")
+    public String updateUserProfile (@PathVariable UUID id, UserEditRequest userEditRequest){
+
+        userService.editUserDetails(id, userEditRequest);
+
+        return "redirect:/home";
     }
 }
---
+++ smart-wallet-application/smart-wallet-application/src/main/java/app/web/dto/UserEditRequest.java
@@ -0,0 +1,17 @@
+package app.web.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserEditRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String profilePicture;
+}
